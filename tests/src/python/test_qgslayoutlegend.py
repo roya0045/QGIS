6f9a633c1d6d01bf446f1c892c74970b7f11104d
Alex <roya0045@users.noreply.github.com>
2019-07-16 18:05:51 -0400

    add symbol_id expression use

--- tests/src/python/test_qgslayoutlegend.py
+++ tests/src/python/test_qgslayoutlegend.py
@@ -425,6 +425,56 @@ class TestQgsLayoutItemLegend(unittest.TestCase, LayoutItemTestCase):
 
         QgsProject.instance().clear()
 
+    def testSymbolExpressions(self):
+        "Test expressions embedded in legend node text"
+        QgsProject.instance().clear()
+        point_path = os.path.join(TEST_DATA_DIR, 'points.shp')
+        point_layer = QgsVectorLayer(point_path, 'points', 'ogr')
+
+        layout = QgsPrintLayout(QgsProject.instance())
+        layout.initializeDefaults()
+
+        map = QgsLayoutItemMap(layout)
+        map.setLayers([point_layer])
+        layout.addLayoutItem(map)
+        map.setExtent(point_layer.extent())
+
+        legend = QgsLayoutItemLegend(layout)
+
+        layer = QgsProject.instance().addMapLayer(point_layer)
+        legendlayer = legend.model().rootGroup().addLayer(point_layer)
+
+        counterTask = point_layer.countSymbolFeatures()
+        counterTask.waitForFinished()
+        TM = QgsApplication.taskManager()
+        actask = TM.activeTasks()
+        print(TM.tasks(), actask)
+        count = actask[0]
+        legend.model().refreshLayerLegend(legendlayer)
+        legendnodes = legend.model().layerLegendNodes(legendlayer)
+        legendnodes[0].setUserLabel('[% @symbol_id %]')
+        legendnodes[1].setUserLabel('[% @symbol_count %]')
+        legendnodes[2].setUserLabel('[% sum("Pilots") %]')
+        label1 = legendnodes[0].evaluateLabel()
+        label2 = legendnodes[1].evaluateLabel()
+        label3 = legendnodes[2].evaluateLabel()
+        count.waitForFinished()
+        self.assertEqual(label1, '0')
+        #self.assertEqual(label2, '5')
+        #self.assertEqual(label3, '12')
+
+        legendlayer.setLabelExpression("Concat(@symbol_label, @symbol_id)")
+
+        label1 = legendnodes[0].evaluateLabel()
+        label2 = legendnodes[1].evaluateLabel()
+        label3 = legendnodes[2].evaluateLabel()
+
+        self.assertEqual(label1, ' @symbol_id 0')
+        #self.assertEqual(label2, '@symbol_count 1')
+        #self.assertEqual(label3, 'sum("Pilots") 2')
+
+        QgsProject.instance().clear()
+
     def testSymbolExpressionRender(self):
         """Test expressions embedded in legend node text"""
         point_path = os.path.join(TEST_DATA_DIR, 'points.shp')
@@ -467,7 +517,7 @@ class TestQgsLayoutItemLegend(unittest.TestCase, LayoutItemTestCase):
         layer_tree_layer.setCustomProperty("legend/title-label", 'bbbb [% 1+2 %] xx [% @layout_name %] [% @layer_name %]')
         QgsMapLayerLegendUtils.setLegendNodeUserLabel(layer_tree_layer, 0, 'xxxx')
         legend.model().refreshLayerLegend(layer_tree_layer)
-        layer_tree_layer.setLabelExpression('Concat(@symbol_id, @symbol_label, count("Class"))')
+        layer_tree_layer.setLabelExpression('Concat(@symbol_id, @symbol_label, count("Class", symbol_id:=@symbol_id))')
         legend.model().layerLegendNodes(layer_tree_layer)[0].setUserLabel(' sym 1')
         legend.model().layerLegendNodes(layer_tree_layer)[1].setUserLabel('[%@symbol_count %]')
         legend.model().layerLegendNodes(layer_tree_layer)[2].setUserLabel('[% count("Class") %]')
