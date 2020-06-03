d72833c372d8c10d9ec4cf33a8078b9ad8284933
Alex RL <roya0045>
2020-06-03 08:00:28 -0400

    Make symbol level aggregate expression possible

--- src/core/vector/qgsvectorlayer.cpp
+++ src/core/vector/qgsvectorlayer.cpp
@@ -4379,6 +4379,15 @@ QVariant QgsVectorLayer::aggregate( QgsAggregateCalculator::Aggregate aggregate,
   if ( ok )
     *ok = false;
 
+  //make a copy or make the param not const
+  QgsAggregateCalculator::AggregateParameters paramCopy = QgsAggregateCalculator::AggregateParameters( parameters );
+  if ( paramCopy.filter.contains( "var('symbol_expression')" ) )
+  {
+    QString symbEx = QgsExpression::replaceExpressionText( "[% ( @symbol_expression ) %]", context );
+    if ( symbEx.isEmpty() )
+      symbEx = "True";
+    paramCopy.filter = paramCopy.filter.replace( "var('symbol_expression')",  symbEx );
+  }
   if ( !mDataProvider )
   {
     return QVariant();
@@ -4394,7 +4403,7 @@ QVariant QgsVectorLayer::aggregate( QgsAggregateCalculator::Aggregate aggregate,
     if ( origin == QgsFields::OriginProvider )
     {
       bool providerOk = false;
-      QVariant val = mDataProvider->aggregate( aggregate, attrIndex, parameters, context, providerOk, fids );
+      QVariant val = mDataProvider->aggregate( aggregate, attrIndex, paramCopy, context, providerOk, fids );
       if ( providerOk )
       {
         // provider handled calculation
@@ -4409,7 +4418,7 @@ QVariant QgsVectorLayer::aggregate( QgsAggregateCalculator::Aggregate aggregate,
   QgsAggregateCalculator c( this );
   if ( fids )
     c.setFidsFilter( *fids );
-  c.setParameters( parameters );
+  c.setParameters( paramCopy );
   return c.calculate( aggregate, fieldOrExpression, context, ok );
 }
 
